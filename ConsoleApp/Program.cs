b097ff90a13a9bfa4fb3e943308bd66e34d636ba
Tyler Parret True <mysteryworldgod@outlook.com>
2026-06-16 22:55:03 +0800

    Fix ConsoleApp to run

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,7 +8,8 @@ namespace ConsoleApp {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello, World!");
 
-			bgm.play(@"C:\Users\Myste\Desktop\m_sys_rouge5_svtheme1.ogg");
+			bgm.open(@"C:\Users\Myste\Desktop\m_sys_rouge5_svtheme1.ogg");
+			bgm.play();
 
 			Console.ReadLine();
 		}
